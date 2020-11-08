c761702a09f63a06fac3cbc6e6ccc6175e7aefb6
Sergey Kozachuk <skozachuk91@yandex.ru>
2020-11-08 23:02:27 +0300

    CONTACTS / Connected script to the form

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 // Form placeholder
 
-const formIputs = documet.querySelectorAll('.form-field');
+const formInputs = document.querySelectorAll('.form-field');
+for (let item of formInputs){
+    const thisPlaceholder = item.nextElementSibling;
+    
+    item.addEventListener('focus', function(){
+        thisPlaceholder.classList.add('active');
+    });
+
+    item.addEventListener('blur', function(){
+        if(item.value == '') {
+            thisPlaceholder.classList.remove('active');
+        }
+    }); 
+}
