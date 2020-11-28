5434f2346ed3dd5af8e50485cc426fad8e1d88f5
Sergey Kozachuk <skozachuk91@yandex.ru>
2020-11-28 20:17:03 +0300

    HEADER MOBILE ADAPTIVE / Connected script to the mobile menu icon

--- js/main.js
+++ js/main.js
@@ -1,113 +1,124 @@
-// Nav page
-$('#page-nav').onePageNav({
-    currentClass: 'active',
-	changeHash: false,
-	scrollSpeed: 750,
-	scrollThreshold: 0.5,
-	filter: '',
-	easing: 'swing',
-	begin: function () { },
-	end: function () { },
-	scrollChange: function ($currentListItem) { }
-});
+$(document).ready(function() {
 
-// Projects filter
-let containerE1 = document.querySelector('#portfolio-projects');
-
-let mixer = mixitup(containerE1, {
-    classNames: {
-        block: ""
-    }
-});
-
-// Form placeholder
-const formInputs = document.querySelectorAll('.form-field');
-for (let item of formInputs){
-    const thisPlaceholder = item.nextElementSibling;
-    
-    item.addEventListener('focus', function(){
-        thisPlaceholder.classList.add('active');
+    // Mobile menu animation
+    const menuToggle = document.querySelector('.toggle-menu');
+    menuToggle.addEventListener('click', function() {
+        this.classList.toggle('active');
+        
     });
 
-    item.addEventListener('blur', function(){
-        if(item.value == '') {
-            thisPlaceholder.classList.remove('active');
-        }
-    }); 
-}
+    // Nav page
+    $('#page-nav').onePageNav({
+        currentClass: 'active',
+        changeHash: false,
+        scrollSpeed: 750,
+        scrollThreshold: 0.5,
+        filter: '',
+        easing: 'swing',
+        begin: function () { },
+        end: function () { },
+        scrollChange: function ($currentListItem) { }
+    });
 
-// Form validate
-$('#contacts-form').validate({
-    rules: {
-        email: {
-            required: true,
-            email: true
-        },
-        subject: {
-            required: true
-        },
-        message: {
-            required: true
-        }
-    },
-    messages: {
-        email: {
-            required: 'Введите email',
-            email: 'отсутсвует символ @'
-        },
-        subject: {
-            required: 'Введите тему сообщения'
-        },
-        message: {
-            required: 'Введите текст сообщения'
-        }
-    },
-    submitHandler: function (form) {
-        ajaxFormSubmit();
-    }
-})
+    // Projects filter
+    let containerE1 = document.querySelector('#portfolio-projects');
 
- // Функция AJAX запроса на сервер
-
- function ajaxFormSubmit() {
-
-    let string = $("#contacts-form").serialize(); // Соханяем данные введенные в форму в строку.
-
-    //Формируем ajax запрос
-    $.ajax({
-        type: "POST", // Тип запроса - POST
-        url: "php/mail.php", // Куда отправляем запрос
-        data: string, // Какие даные отправляем, в данном случае отправляем переменную string
-
-        // Функция если все прошло успешно
-        success: function (html) {
-            $("#contacts-form").slideUp(800);
-            $('#answer').html(html);
+    let mixer = mixitup(containerE1, {
+        classNames: {
+            block: ""
         }
     });
 
-    // Чтобы по Submit больше ничего не выполнялось - делаем возврат false чтобы прервать цепчку срабатывания остальных функций
-    return false;
-}
+    // Form placeholder
+    const formInputs = document.querySelectorAll('.form-field');
+    for (let item of formInputs){
+        const thisPlaceholder = item.nextElementSibling;
+        
+        item.addEventListener('focus', function(){
+            thisPlaceholder.classList.add('active');
+        });
 
-// Back top button
-$('#backTop').hide();
-$(window).scroll( function () {
-    if($(this).scrollTop() > 600){
-        $('#backTop').fadeIn();
+        item.addEventListener('blur', function(){
+            if(item.value == '') {
+                thisPlaceholder.classList.remove('active');
+            }
+        }); 
     }
-    else{
-        $('#backTop').fadeOut();
-    }
-})
 
-// Fixed header menu
-const fixMenu = document.querySelector('.fix-menu');
-window.addEventListener('scroll', function(){
-    if(this.pageYOffset > 10){
-        fixMenu.classList.add('active');
+    // Form validate
+    $('#contacts-form').validate({
+        rules: {
+            email: {
+                required: true,
+                email: true
+            },
+            subject: {
+                required: true
+            },
+            message: {
+                required: true
+            }
+        },
+        messages: {
+            email: {
+                required: 'Введите email',
+                email: 'отсутсвует символ @'
+            },
+            subject: {
+                required: 'Введите тему сообщения'
+            },
+            message: {
+                required: 'Введите текст сообщения'
+            }
+        },
+        submitHandler: function (form) {
+            ajaxFormSubmit();
+        }
+    })
+
+    // Функция AJAX запроса на сервер
+
+    function ajaxFormSubmit() {
+
+        let string = $("#contacts-form").serialize(); // Соханяем данные введенные в форму в строку.
+
+        //Формируем ajax запрос
+        $.ajax({
+            type: "POST", // Тип запроса - POST
+            url: "php/mail.php", // Куда отправляем запрос
+            data: string, // Какие даные отправляем, в данном случае отправляем переменную string
+
+            // Функция если все прошло успешно
+            success: function (html) {
+                $("#contacts-form").slideUp(800);
+                $('#answer').html(html);
+            }
+        });
+
+        // Чтобы по Submit больше ничего не выполнялось - делаем возврат false чтобы прервать цепчку срабатывания остальных функций
+        return false;
     }
-    else{
-        fixMenu.classList.remove('active');    
-    }
+
+    // Back top button
+    $('#backTop').hide();
+    $(window).scroll( function () {
+        if($(this).scrollTop() > 600){
+            $('#backTop').fadeIn();
+        }
+        else{
+            $('#backTop').fadeOut();
+        }
+    })
+
+    // Fixed header menu
+    const fixMenu = document.querySelector('.fix-menu');
+    window.addEventListener('scroll', function(){
+        if(this.pageYOffset > 10){
+            fixMenu.classList.add('active');
+        }
+        else{
+            fixMenu.classList.remove('active');    
+        }
+    })
+
 })
